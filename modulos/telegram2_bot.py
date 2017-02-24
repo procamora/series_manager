08d8d88b2926ec2568aa92e7fecb7d6e6ae90c1d
procamora <pablojoserocamora@gmail.com>
2017-02-24 17:17:04 +0100

    cambiado system por subprocess

--- modulos/telegram2_bot.py
+++ modulos/telegram2_bot.py
@@ -26,7 +26,8 @@ def datosIniciales():
     with open(r'{}/id.conf'.format(directorio_local), 'r') as f:
         id_fich = f.readline().replace('/n', '')
 
-    query = 'SELECT * FROM Credenciales'.format(id_fich)
+    query = 'SELECT * FROM Configuraciones, Credenciales WHERE ID LIKE {} LIMIT 1'.format(
+        id_fich)
     return conectionSQLite(ruta_db, query, True)[0]
 
 
@@ -62,7 +63,7 @@ def checkError(codigo, stderr):
     return False
 
 
-def descargaTorrent(direcc):  # PARA NEWPCT1
+def descargaTorrent(direcc, message):  # PARA NEWPCT1
     '''
     Funcion que obtiene la url torrent del la dirreccion que recibe
 
@@ -82,9 +83,10 @@ def descargaTorrent(direcc):  # PARA NEWPCT1
         session = requests.session()
         page = session.get(direcc, verify=False).text
         sopa = BeautifulSoup(page, 'html.parser')
-        #print(sopa.findAll('div', {"id": "tab1"}))
+        # print(sopa.findAll('div', {"id": "tab1"}))
         print(sopa.find_all("a", class_="btn-torrent")[0]['href'])
         return sopa.find('div', {"id": "tab1"}).a['href']
+
 
 def descargaFichero(url, destino):
     r = requests.get(url)
@@ -122,137 +124,194 @@ def command_system(message):
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['cron_gestor_series'])
 def send_cgs(message):
     bot.reply_to(message, 'Ejecutado con gs')
-    f = os.popen(
-        'cd /home/pi/Gestor-de-Series/ && /usr/bin/python3 /home/pi/Gestor-de-Series/descarga_automatica_cli.py')
-    now = f.read()
-    if len(now) == 0:
+
+    comando = 'cd /home/pi/Gestor-de-Series/ && /usr/bin/python3 /home/pi/Gestor-de-Series/descarga_automatica_cli.py'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
         bot.reply_to(
             message, 'Ejecutado, puede que haya fallado o sea muy largo el resultado')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['cron_rsync_samba'])
 def send_crs(message):
-    f = os.popen(
-        'cd /home/pi/scripts && /usr/bin/python3 /home/pi/scripts/rsync_samba.py')
-    now = f.read()
-    if len(now) == 0:
+    comando = 'cd /home/pi/scripts && /usr/bin/python3 /home/pi/scripts/rsync_samba.py'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
         bot.reply_to(message, 'Ejecutado rsync')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['mount_a'])
 def send_mount(message):
-    f = os.popen('sudo mount -a')
-    now = f.read()
-    if len(now) == 0:
+    comando = 'sudo mount -a'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
         bot.reply_to(message, 'Ejecutado mount')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['reboot_transmission'])
 def send_ts(message):
-    f = os.popen('sudo /etc/init.d/transmission-daemon restart')
-    now = f.read()
-    if len(now) == 0:
+    comando = 'sudo /etc/init.d/transmission-daemon restart'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
         bot.reply_to(message, 'Ejecutado  reboot ts')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['reboot_system'])
 def send_sys(message):
     bot.reply_to(message, 'Se ejecutara en breve el reboot')
-    os.popen('sudo reboot')
+
+    comando = 'sudo reboot'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['df_h'])
 def send_df(message):
-    f = os.popen('df -h')
-    now = f.read()
-    if len(now) == 0:
+    comando = 'df -h'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
         bot.reply_to(message, 'Ejecutado df -h')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['info'])
 def send_info(message):
     # uptime, cpu, ram, disk, speed download/upload
-    f = os.popen('pwd')
-    now = f.read()
-    if len(now) == 0:
+    comando = 'pwd'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stderr) == 0:
         bot.reply_to(message, 'Ejecutado, pero esta en proceso')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['show_torrent'])
 def send_show_torrent(message):
     #f = os.popen('transmission-remote --auth=pi:{} -l | egrep -v "Finished|Stopped|Seeding|ID|Sum:" |  awk \'{print $2}{for(i=13; i<=NF; i++) printf "%s",$i (i==NF?ORS:OFS)}\''.format(pass_transmission))
-    f = os.popen(
-        'transmission-remote 127.0.0.1:9091 --auth=pi:{} -l | egrep -v "Finished|Stopped|Seeding|ID|Sum:"'.format(pass_transmission))
-    now = f.read()
-    if len(now) != 0:
-        for line in now:
+    comando = 'transmission-remote 127.0.0.1:9091 --auth=pi:{} -l | egrep -v "Finished|Stopped|Seeding|ID|Sum:"'.format(
+        pass_transmission)
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) != 0:
+        for line in stdout:
             line = re.sub(
-                '\[AC3 5\.1-Castellano-AC3 5.1 Ingles\+Subs\]|\[ES-EN\]|\[AC3 5.1 Español Castellano\]|\[HDTV 720p?\]|(\d+\.?\d+|None)( )+(MB|GB|kB|Unknown).*(Up & Down|Downloading|Queued|Idle|Uploading)( )*| - Temporada \d+ |(\d+\.\d+)( )+(\d+\.\d+)', '', now)
+                '\[AC3 5\.1-Castellano-AC3 5.1 Ingles\+Subs\]|\[ES-EN\]|\[AC3 5.1 Español Castellano\]|\[HDTV 720p?\]|(\d+\.?\d+|None)( )+(MB|GB|kB|Unknown).*(Up & Down|Downloading|Queued|Idle|Uploading)( )*| - Temporada \d+ |(\d+\.\d+)( )+(\d+\.\d+)', '', stdout)
         bot.reply_to(message, line)
-        #bot.reply_to(message, now)
     else:
         bot.reply_to(message, 'Ningun torrent activo en este momento.')
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, commands=['exit'])
 def send_exit(message):
-    f = os.popen('pwd')
-    now = f.read()
-    if len(now) == 0:
-        bot.reply_to(message, 'Ejecutado,no hace nada')
+    comando = 'pwd'
+    ejecucion = subprocess.Popen(
+        comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = ejecucion.communicate()
+
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    elif len(stdout) == 0:
+        bot.reply_to(message, 'Ejecutado, no hace nada')
     else:
-        bot.reply_to(message, now)
+        bot.reply_to(message, stdout)
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, regexp="[Cc]md: .*")
 def handle_cmd(message):
     peligro = ['sudo reboot', ':(){ :|: & };:']
 
-    msg = re.sub('[Cc]md: ', '', message.text)
-    if not msg in peligro:
-        f = os.popen(msg)
-        now = f.read()
-        if len(now) == 0:
-            bot.reply_to(message, 'Ejecutado: {}'.format(msg))
+    comando = re.sub('[Cc]md: ', '', message.text)
+    if comando not in peligro:
+
+        ejecucion = subprocess.Popen(
+            comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = ejecucion.communicate()
+
+        if checkError(ejecucion, stderr):
+            bot.reply_to(message, 'Error: {}'.format(stderr))
+            return
+        elif len(stdout) == 0:
+            bot.reply_to(message, 'Ejecutado: {}'.format(comando))
         else:
-            bot.reply_to(message, now)
+            bot.reply_to(message, stdout)
     else:
         bot.reply_to(message, 'Comando aun no implementado')
 
 
 @bot.message_handler(func=lambda message: message.chat.id == administrador, regexp="^magnet:\?xt=urn.*")
 def handle_magnet(message):
+    bot.reply_to(message, 'Ejecutado add torrent')
 
     comando = 'transmission-remote 127.0.0.1:9091 --auth=pi:{} --add "{}"'.format(
         pass_transmission, message.text)
-    if modo_debug:
-        print(comando)
-    os.popen(comando)
-
     ejecucion = subprocess.Popen(
         comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = ejecucion.communicate()
 
-    checkError(ejecucion, stderr)
     if modo_debug:
-        bot.reply_to(message, 'Exito: {}'.format(formatea(stdout)))
-        bot.reply_to(message, 'Error: {}'.format(
-            checkError(ejecucion, stderr)))
+        print(comando)
 
-    bot.reply_to(message, 'Ejecutado add torrent')
-    send_show_torrent(message)
+    if checkError(ejecucion, stderr):
+        bot.reply_to(message, 'Error: {}'.format(stderr))
+        return
+    else:
+        send_show_torrent(message)
 
 
 @bot.message_handler(regexp="^(http://)?www.(newpct1|tumejortorrent).com/.*")
@@ -265,7 +324,7 @@ def handle_newpct1(message):
         urlPeli = re.sub('(http://)?www.newpct1.com/',
                          'http://www.newpct1.com/descarga-torrent/', message.text)
 
-    url = descargaTorrent(urlPeli)
+    url = descargaTorrent(urlPeli, message)
     if url is not None:
         fich = '/tmp/{}.torrent'.format(message.chat.id)
         descargaFichero(url, fich)
@@ -273,7 +332,8 @@ def handle_newpct1(message):
         file_data = open(fich, 'rb')
         bot.send_document(message.chat.id,  file_data)
         if message.chat.id == administrador:
-            os.rename(fich, '/home/pi/Downloads/file.torrent')
+            os.rename(
+                fich, '{}/file.torrent'.format(credenciales['RutaDescargas']))
         else:
             pass
         with open('/tmp/descarga_torrent.log', "a") as f:
@@ -324,7 +384,7 @@ def my_document(message):
         file_info = bot.get_file(message.document.file_id)
         #bot.send_message(message.chat.id, 'https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path))
         file = requests.get(
-            'https://api.telegram.org/file/bot{0}/{1}'.format(api_telegram, file_info.file_path))
+            'https://api.telegram.org/file/bot{0}/{1}'.format(credenciales['api_telegram'], file_info.file_path))
         with open('/home/pi/Downloads/{}.torrent'.format(message.document.file_id), "wb") as code:
             code.write(file.content)
         bot.reply_to(
