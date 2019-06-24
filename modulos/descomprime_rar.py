103836172cd397bb1a99ab8bb9d171b8f6eb62b2
procamora <pablojoserocamora@gmail.com>
2019-06-24 18:34:19 +0200

    descomprime solo manda mensaje para la parte1

--- modulos/descomprime_rar.py
+++ modulos/descomprime_rar.py
@@ -31,16 +31,16 @@ def descomprime(fichero):
     # http://librosweb.es/libro/python/capitulo_6/metodos_de_union_y_division.html
     # partition(fichero.rar) = (antecedente, fichero.rar, consecuente), lo que
     # hago es sacar el directorio del fichero
-    dir = fichero.partition(fichero.split('/')[-1])[0]
+    directory = fichero.partition(fichero.split('/')[-1])[0]
     rar = rarfile.RarFile(fichero)
     url = str()
     if rar.needs_password():
         # busco la url para descargar la pass
         # con glob no me lista los ficheros
-        for i in os.listdir('{}'.format(dir)):
+        for i in os.listdir('{}'.format(directory)):
             if i == 'CONTRASEÑA PARA DESCOMPRIMIR.txt':
                 texto = str()
-                with open('{}{}'.format(dir, i), 'r', encoding="ISO-8859-1") as f:
+                with open('{}{}'.format(directory, i), 'r', encoding="ISO-8859-1") as f:
                     texto = f.read()
                 url = re.findall("((https?\:\/\/)?(\w+\.)+\w{2,3}\/?.*\/)", texto)
 
@@ -48,32 +48,28 @@ def descomprime(fichero):
             passwd = busca_pass(url[0][0])
             rar.setpassword(passwd)  # whatever the password is
 
-    peli = str()
-    for i in rar.namelist():
-        if re.search("mkv", i):
-            peli = i
-
-    rar.extract(member=peli, path=dir)
+    rar.extractall(path=directory)
     os.remove(fichero)
-    #rar.extractall(path=dir, pwd=passwd)
 
 
 def main(ruta):
     a = telegram2.TG2('33063767')
     for i in glob.glob(ruta):
-        #print(i)
+        print(i)
         try:
             a.sendTg(
                 'Empieza proceos descomprimir: {}'.format(i.split('/')[-1]))
             descomprime(i)
             a.sendTg(
                 'Termina proceos descomprimir: {}'.format(i.split('/')[-1]))
-        except:
-            a.sendTg('Fallo al descomprimir: {}'.format(i.split('/')[-1]))
+        except Exception as e:
+            if str(e) != "Need to start from first volume":
+                a.sendTg('Fallo al descomprimir: {} por: {}'.format(i.split('/')[-1], e))
+            print("|{}|".format(e))
 
             with open('/tmp/unrar.log', 'a', encoding="UTF-8") as f:
-                f.write(sys.exc_info()[0])
+                f.write(str(sys.exc_info()[0]))
 
 
 if __name__ == '__main__':
-    main('/media/640Gb/*/*.rar')
+    main('/media/pi/640Gb/*/*.rar')
