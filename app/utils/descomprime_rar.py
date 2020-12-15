405d3f803ed24422ccc0bdb04b9bd6358d7e5cef
Pablo <pablojoserocamora@gmail.com>
2020-12-15 21:00:18 +0100

    Update descomprime_rar.py

--- app/utils/descomprime_rar.py
+++ app/utils/descomprime_rar.py
@@ -4,10 +4,13 @@
 """
 sudo apt-get install unrar-free
 """
+
 import glob
 import os
 import re
 import sys
+from pathlib import Path
+from typing import Text, List, NoReturn
 
 import rarfile
 import requests
@@ -17,30 +20,25 @@ from app import logger
 from app.utils.telegram2 import Telegram
 
 
-def search_pass(url):
+def search_pass(url: Text) -> Text:
     """
     funcion para buscar la password en la url dada, parsea todos el html
     """
-    session = requests.session()
-    page = session.get(url, verify=False).text
-    sopa = BeautifulSoup(page, 'html.parser')
+    session: requests.sessions = requests.session()
+    page: Text = session.get(url, verify=False).text
+    sopa: BeautifulSoup = BeautifulSoup(page, 'html.parser')
 
     return sopa.find('input', {"id": "txt_password"})['value']
 
 
-def unzip(fichero):
-    # http://librosweb.es/libro/python/capitulo_6/metodos_de_union_y_division.html
-    # partition(fichero.rar) = (antecedente, fichero.rar, consecuente), lo que
-    # hago es sacar el directorio del fichero
-    directory = fichero.partition(fichero.split('/')[-1])[0]
-    rar = rarfile.RarFile(fichero)
-    url = str()
+def extract_all_parts(file_rar: Path) -> NoReturn:
+    rar: rarfile.RarFile = rarfile.RarFile(file_rar)
+    url: List = list()
     if rar.needs_password():
-        # busco la url para descargar la pass
-        # con glob no me lista los ficheros
-        for i in os.listdir(directory):
+        # busco la url para descargar la pass con glob no me lista los ficheros
+        for i in os.listdir(file_rar.parent):
             if i == 'CONTRASEÑA PARA DESCOMPRIMIR.txt':
-                with open(f'{directory}{i}', 'r', encoding="ISO-8859-1") as f:
+                with open(f'{file_rar.parent}{i}', 'r', encoding="ISO-8859-1") as f:
                     texto = f.read()
                 url = re.findall(r"((https?://)?(\w+\.)+\w{2,3}/?.*/)", texto)
 
@@ -48,26 +46,32 @@ def unzip(fichero):
             passwd = search_pass(url[0][0])
             rar.setpassword(passwd)  # whatever the password is
 
-    rar.extractall(path=directory)
-    os.remove(fichero)
+    rar.extractall(path=str(file_rar.parent))
+    name_file: Text = Path(file_rar.stem).stem  # remove .part1.rar
+    all_parts: Text = str(Path(file_rar.parent, name_file))
+    for i in glob.glob(f'{all_parts}*'):
+        logger.debug(f"- remove {i}")
+        os.remove(i)
 
 
 def main(ruta):
-    a = Telegram('33063767')
+    tg: Telegram = Telegram('33063767')
     for i in glob.glob(ruta):
-        logger.info(i)
+        if not re.search(r'part1.rar', i):  # skip those parts that are not part1.rar
+            continue
+
+        logger.debug(i)
         try:
-            a.send_tg(f'Empieza proceos descomprimir: {i.split("/")[-1]}')
-            unzip(i)
-            a.send_tg(f'Termina proceos descomprimir: {i.split("/")[-1]}')
+            # tg.send_tg(f'Start the process of decompressing: {i.split("/")[-1]}')
+            extract_all_parts(Path(i))
+            # tg.send_tg(f'End the process of decompressing: {i.split("/")[-1]}')
         except Exception as e:
-            if str(e) != "Need to start from first volume":
-                a.send_tg(f'Fallo al descomprimir: {i.split("/")[-1]} por: {e}')
-            logger.info(f"|{e}|")
+            tg.send_tg(f'Fallo al descomprimir: {i} por: {e}')
+            logger.error(f"|{e}|")
 
             with open('/tmp/unrar.log', 'a', encoding="UTF-8") as f:
                 f.write(str(sys.exc_info()[0]))
 
 
 if __name__ == '__main__':
-    main('/media/pi/640Gb/*/*.rar')
+    main('/home/procamora/Documents/kk/*/*.rar')
